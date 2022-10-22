e8a3b7d9b21ae7b3ed8b3ef8214f73df148430b5
linqingfeng <loose.lqf@jeeseen.com>
2022-10-22 11:24:18 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -73,7 +73,7 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    requires = { 'kyazdani42/nvim-web-devicons' },
     config = function()
       require('lualine').setup()
     end
