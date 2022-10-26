a8ce26b77dda785db477410eae930b8cbf890df0
loose <lqfxz520@gmail.com>
2022-10-27 00:36:04 +0800

    update

--- lua/basic.lua
+++ lua/basic.lua
@@ -20,6 +20,10 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 vim.g.mapleader = ' '
 vim.o.pumheight = 20
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 vim.g.coc_global_extensions = {
   'coc-explorer',
   'coc-json',
