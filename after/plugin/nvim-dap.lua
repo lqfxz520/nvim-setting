18499571775d93c1f5e5581108566dc136c93397
linqingfeng <loose.lqf@jeeseen.com>
2023-02-28 17:01:31 +0800

    feat: add debugging plugin

--- after/plugin/nvim-dap.lua
+++ after/plugin/nvim-dap.lua
@@ -7,8 +7,60 @@ if not status then
   return
 end
 
+local vscodeStatus, dapVscode = pcall(require, "dap-vscode-js")
+if not vscodeStatus then
+  vim.notify("not found dap-vscode-js")
+  return
+end
+
+local uiStatus, dapui = pcall(require, "dapui")
+if not uiStatus then
+  vim.notify("not found dapui")
+  return
+end
+
 local opts = { silent = true }
-nnoremap("<A-;>", function () dap.toggle_breakToggle() end, opts)
+nnoremap("<A-;>", function () dap.toggle_breakpoint() end, opts)
 nnoremap("<A-,>", function () dap.continue() end, opts)
 nnoremap("<A-[>", function () dap.step_into() end, opts)
 nnoremap("<A-]>", function () dap.step_over() end, opts)
+
+dapVscode.setup({
+  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+  debugger_path = os.getenv('HOME') .. "/dev/vscode-js-debug", -- Path to vscode-js-debug installation.
+  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+  log_file_path = "/tmp/dap_vscode_js.log", -- Path for file logging
+  log_file_level = 1, -- Logging level for output to file. Set to false to disable file logging.
+  log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+})
+
+for _, language in ipairs({ "typescript", "javascript" }) do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = require'dap.utils'.pick_process,
+      cwd = "${workspaceFolder}",
+    }
+  }
+end
+
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
--- lua/restart/packer.lua
+++ lua/restart/packer.lua
@@ -20,6 +20,9 @@ return require('packer').startup(function(use)
   use { 'iamcco/markdown-preview.nvim', run = 'cd app && npm install',
     setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }
 
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use { "mxsdev/nvim-dap-vscode-js", requires = {"mfussenegger/nvim-dap"} }
+
   use 'tpope/vim-commentary'
 
   use 'JoosepAlviste/nvim-ts-context-commentstring'
